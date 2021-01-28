18dbc6a25cdd7c8101e1d0126da9bb3759e5c206
lightsaberka <xvera6@gmail.com>
2021-01-28 23:43:21 +0000

    :tada: Init task 2 and 3

---
+++ task2/main.cu
@@ -0,0 +1,32 @@
+/**
+ * Nasobeni matic v globalni pameti.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <cuda.h>
+#include <cuda_runtime.h>
+#include <iostream>
+
+/**
+ * This macro checks return value of the CUDA runtime call and exits
+ * the application if the call failed.
+ */
+#define CUDA_CHECK_RETURN(value) {                                                                          \
+                                                                                                            \
+	cudaError_t err = value;                                                                                \
+                                                                                                            \
+	if (err != cudaSuccess) {                                                                               \
+		fprintf(stderr, "Error %s at line %d in file %s\n", cudaGetErrorString(err), __LINE__, __FILE__);   \
+		exit(1);                                                                                            \
+	}                                                                                                       \
+}                                                                                                           \
+
+#define BLOCK_SIZE (16u)
+
+/**
+ * Host function that prepares data array and passes it to the CUDA kernel.
+ */
+int main(int argc, char **argv) {
+	
+	return 0;
+}
---
+++ task3/main.cu
@@ -0,0 +1,32 @@
+/**
+ * Zpracovani obrazu pomoci 2D konvoluce.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <cuda.h>
+#include <cuda_runtime.h>
+#include <iostream>
+
+/**
+ * This macro checks return value of the CUDA runtime call and exits
+ * the application if the call failed.
+ */
+#define CUDA_CHECK_RETURN(value) {                                                                          \
+                                                                                                            \
+	cudaError_t err = value;                                                                                \
+                                                                                                            \
+	if (err != cudaSuccess) {                                                                               \
+		fprintf(stderr, "Error %s at line %d in file %s\n", cudaGetErrorString(err), __LINE__, __FILE__);   \
+		exit(1);                                                                                            \
+	}                                                                                                       \
+}                                                                                                           \
+
+#define VECTOR_SIZE (1234u)
+
+/**
+ * Host function that prepares data array and passes it to the CUDA kernel.
+ */
+int main(int argc, char **argv) {
+
+	return 0;
+}
